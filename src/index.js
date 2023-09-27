6c8cc9949a1b532fbc30848bfbfe11b5b1af51c1
JackRakham <j.villatet@uniandes.edu.co>
2023-09-26 20:59:40 -0500

    Actividad completa

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import messagesES from "./locales/es";
+import {IntlProvider} from 'react-intl';
 import JobsList from "./components/jobslist";
+import messagesEN from './locales/en';
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+
+
+const userLanguage = navigator.language || navigator.userLanguage;
+const messages = userLanguage.startsWith('es-ES') ? messagesES : messagesEN;
+
+ReactDOM.render(
+    <IntlProvider locale={userLanguage} messages= {messages}>
+            <JobsList/>
+            
+    </IntlProvider>, document.getElementById("root")
+);
